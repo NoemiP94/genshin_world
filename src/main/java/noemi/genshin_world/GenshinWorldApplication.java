3b3f7ac934af2bfa80124bd536e76d8b5bd89e7c
NoemiP94 <m_emi94@hotmail.it>
2024-04-26 11:03:32 +0200

    Initial commit

---
+++ src/main/java/noemi/genshin_world/GenshinWorldApplication.java
@@ -0,0 +1,13 @@
+package noemi.genshin_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noemi/genshin_world/GenshinWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package noemi.genshin_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenshinWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
