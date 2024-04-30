95597d98411b4273bd3a0fed467a37549edf0ca6
NoemiP94 <m_emi94@hotmail.it>
2024-04-30 11:50:27 +0200

    add repositories

---
+++ src/main/java/noemi/genshin_world/repositories/CharacterDAO.java
@@ -0,0 +1,10 @@
+package noemi.genshin_world.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CharacterDAO extends JpaRepository<Character, UUID> {
+}
---
+++ src/main/java/noemi/genshin_world/repositories/DegreeDAO.java
@@ -0,0 +1,11 @@
+package noemi.genshin_world.repositories;
+
+import noemi.genshin_world.entities.Degree;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DegreeDAO extends JpaRepository<Degree, UUID> {
+}
