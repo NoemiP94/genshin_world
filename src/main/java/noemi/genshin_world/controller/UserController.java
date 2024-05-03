0fd6308115c122b575ad89bdcea7bc3cbd2f0a71
NoemiP94 <m_emi94@hotmail.it>
2024-05-03 11:13:09 +0200

    add UserController

---
+++ src/main/java/noemi/genshin_world/controller/UserController.java
@@ -0,0 +1,40 @@
+package noemi.genshin_world.controller;
+
+import noemi.genshin_world.entities.User;
+import noemi.genshin_world.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Page<User> getAllUsers(@RequestParam(defaultValue = "0") int page,
+                                  @RequestParam(defaultValue = "10") int size,
+                                  @RequestParam(defaultValue = "id") String orderBy){
+        return userService.getUsers(page, size, orderBy);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public User findUserById(@PathVariable UUID id){
+        return userService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void getUserByIdAndDelete(@PathVariable UUID id){
+        userService.deleteById(id);
+    }
+
+}
