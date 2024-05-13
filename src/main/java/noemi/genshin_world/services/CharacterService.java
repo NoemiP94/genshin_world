f899f63a60f62c314128e470d9a69c2414753e5b
NoemiP94 <m_emi94@hotmail.it>
2024-05-13 11:42:37 +0200

    add other save methods to CharacterService

--- src/main/java/noemi/genshin_world/services/CharacterService.java
+++ src/main/java/noemi/genshin_world/services/CharacterService.java
@@ -1,15 +1,21 @@
 package noemi.genshin_world.services;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import noemi.genshin_world.entities.*;
 import noemi.genshin_world.entities.Character;
-import noemi.genshin_world.entities.Region;
 import noemi.genshin_world.entities.enums.VisionType;
 import noemi.genshin_world.entities.enums.WeaponType;
 import noemi.genshin_world.exceptions.IllegalArgumentException;
+import noemi.genshin_world.exceptions.NotFoundException;
 import noemi.genshin_world.payloads.character.CharacterDTO;
 import noemi.genshin_world.repositories.CharacterDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class CharacterService {
@@ -21,6 +27,10 @@ public class CharacterService {
     private RegionService regionService;
     @Autowired
     private ArtifactSetService artifactSetService;
+    @Autowired
+    private WeaponService weaponService;
+    @Autowired
+    private MaterialService materialService;
 
     //save
     public Character saveCharacter(CharacterDTO body){
@@ -46,13 +56,55 @@ public class CharacterService {
     }
 
     //addArtifactSet
-
+    public void addArtifactSetToCharacter(UUID characterId, UUID artifactSetId){
+        Character character = characterDAO.findById(characterId).orElseThrow(()-> new NotFoundException(characterId));
+        ArtifactSet artifactSet = artifactSetService.findById(artifactSetId);
+        character.getArtifactSetList().add(artifactSet);
+        characterDAO.save(character);
+    }
     //removeArtifactSet
+    public void removeArtifactSetFromCharacter(UUID characterId, UUID artifactSetId){
+        Character character = characterDAO.findById(characterId).orElseThrow(()-> new NotFoundException(characterId));
+        ArtifactSet artifactSet = artifactSetService.findById(artifactSetId);
+        character.getArtifactSetList().remove(artifactSet);
+        characterDAO.save(character);
+    }
     //addWeapon
+    public void addWeaponToCharacter(UUID characterId, UUID weaponId){
+        Character character = characterDAO.findById(characterId).orElseThrow(()-> new NotFoundException(characterId));
+        Weapon weapon = weaponService.findById(weaponId);
+        character.getFavWeapons().add(weapon);
+        characterDAO.save(character);
+    }
     //removeWeapon
+    public void removeWeaponFromCharacter(UUID characterId, UUID weaponId){
+        Character character = characterDAO.findById(characterId).orElseThrow(()-> new NotFoundException(characterId));
+        Weapon weapon = weaponService.findById(weaponId);
+        character.getFavWeapons().remove(weapon);
+        characterDAO.save(character);
+    }
     //addMaterial
+    public void addMaterialToCharacter(UUID characterId, UUID materialId){
+        Character character = characterDAO.findById(characterId).orElseThrow(()-> new NotFoundException(characterId));
+        Material material = materialService.findById(materialId);
+        character.getAscensionMaterials().add(material);
+        characterDAO.save(character);
+    }
     //removeMaterial
+    public void removeMaterialFromCharacter(UUID characterId, UUID materialId){
+        Character character = characterDAO.findById(characterId).orElseThrow(()-> new NotFoundException(characterId));
+        Material material = materialService.findById(materialId);
+        character.getAscensionMaterials().remove(material);
+        characterDAO.save(character);
+    }
     //uploadImage
+    public String uploadImage(UUID id, MultipartFile file) throws IOException {
+        Character character = characterDAO.findById(id).orElseThrow(()-> new NotFoundException(id));
+        String url = (String) cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap()).get("url");
+        character.setImage(url);
+        characterDAO.save(character);
+        return url;
+    }
     //findAll
     //findById
     //update
