79adcfcf10b9c6cde478951b9ec4617186291b2b
NoemiP94 <m_emi94@hotmail.it>
2024-05-13 12:03:27 +0200

    add dao methods in CharacterDAO

--- src/main/java/noemi/genshin_world/repositories/CharacterDAO.java
+++ src/main/java/noemi/genshin_world/repositories/CharacterDAO.java
@@ -1,11 +1,23 @@
 package noemi.genshin_world.repositories;
 
 import noemi.genshin_world.entities.Character;
+import noemi.genshin_world.entities.enums.VisionType;
+import noemi.genshin_world.entities.enums.WeaponType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CharacterDAO extends JpaRepository<Character, UUID> {
+    Optional<Character> findByName(String name);
+    Page<Character> findByVisionType(VisionType visionType, Pageable pageable);
+    Page<Character> findByWeaponType(WeaponType weaponType, Pageable pageable);
+    Page<Character> findByRegionId(UUID regionId, Pageable pageable);
+    Page<Character> findByArtifactSetId(UUID artifactSetId, Pageable pageable);
+    Page<Character> findByWeaponId(UUID weaponId, Pageable pageable);
+    Page<Character> findByMaterialId(UUID materialId, Pageable pageable);
 }
